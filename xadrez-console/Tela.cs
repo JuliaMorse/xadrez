a1f9fea2edfd3388aee2d8abe74203128cf20a41
Julia Morse <juliamorsef@gmail.com>
2019-11-04 10:10:08 -0300

    Fazendo os primeiros testes de movimentacao

--- xadrez-console/Tela.cs
+++ xadrez-console/Tela.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using xadrez;
 using tabuleiro;
 
 namespace xadrez_console
@@ -19,13 +19,21 @@ namespace xadrez_console
                     }
                     else
                     {
-                        Tela.imprimirPeca(tab.peca(i, j));
+                        imprimirPeca(tab.peca(i, j));
                         Console.Write(" ");
                     }
                 }
                 Console.WriteLine();
             }
             Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static PosicaoXadrez lerPosicaoXadrez()
+        {
+            string s = Console.ReadLine();
+            char coluna= s[0];
+            int linha = int.Parse(s[1] + "");
+            return new PosicaoXadrez(coluna, linha);
         }
 
         public static void imprimirPeca(Peca peca)
